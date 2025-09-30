0644014350cb45d652cef33bb4d69ffe80b38b40
sanketphadatare-18 <sanketphadatare1718@outlook.com>
2025-09-30 17:12:43 +0530

    added new feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './Models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ang-CRUD-usingSingleComponent';
+
+  users: User[] = [
+    { userid: 1, name: 'Sanket', city: 'Pune' },
+    { userid: 2, name: 'Rohit', city: 'Mumbai' },
+    { userid: 3, name: 'Rahul', city: 'Banglore' },
+    { userid: 4, name: 'Karan', city: 'Chennai' }
+  ]
+
+  user: User = new User();
+
+  isEdit: boolean = false;
+
+
+  addUser() {
+
+    this.users.push(this.user);
+    // alert("User Added Successfully");
+    this.user = new User();
+  }
+
+  editUser(id: number) {
+    let foundUser: User | any = this.users.find((u) => u.userid == id);
+    this.user = foundUser;
+    this.isEdit = true;
+  }
+
+
+
+  updateUser(id: number) {
+    let existingUser: User | any = this.users.find(u => u.userid == id);
+    let index = this.users.indexOf(existingUser);
+
+    this.users[index].name = this.user.name;
+    this.users[index].city = this.user.city;
+
+    this.isEdit = false;
+    this.user = new User();
+    // alert
+    // 
+    ("User Updated Successfully");
+  }
+
+
+  deleteUser(id: number) {
+    let existingUser: User | any = this.users.find(u => u.userid == id);
+    let index = this.users.indexOf(existingUser);
+
+    // alert("User Deleted Successfully");
+    if (confirm("Are you sure to delete?")) {
+      this.users.splice(index, 1);
+      // alert("User Deleted Successfully");
+    }
+
+  }
+
+
+
+
 }
